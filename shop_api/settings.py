d08499e17e966c0f3a0f4e18390ded304c0d9530
ArslanGeek <bra.kreedz@gmail.com>
2023-12-15 16:57:31 +0600

    eighth homework

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -88,12 +88,13 @@ WSGI_APPLICATION = 'shop_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': os.environ.get('DB_NAME'),
+        # 'USER': os.environ.get('DB_USER'),
+        # 'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
